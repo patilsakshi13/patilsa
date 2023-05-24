462fbf95cfdbb3ea6172a51f6feb7b70986d8bdd
HP <HP@DESKTOP-IU8RUFA>
2023-05-24 13:02:35 +0530

    By sakshi

--- GitNewDemo/src/com/cjc/iptech/Demo.java
+++ GitNewDemo/src/com/cjc/iptech/Demo.java
@@ -1,8 +1,18 @@
 package com.cjc.iptech;
 
-public class Demo {
-public static void main(String[] args) {
-	System.out.println("hello");
-	System.out.println("cjc");
-}
+public class Demo 
+{
+	public void m1()
+	{
+		System.out.println("m1___Demo");
+	}
+	public Demo()
+	{
+		System.out.println("Constructor demo");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		System.out.println("cjc");
+	}
 }
